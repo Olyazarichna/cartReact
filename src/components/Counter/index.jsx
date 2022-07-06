f82f16293a4e692185624e25b25057c7f5e0a6b7
Olyazarichna <olya.zarichna@gmail.com>
2022-07-06 18:31:45 +0200

    cart

---
+++ src/components/Counter/index.jsx
@@ -0,0 +1,25 @@
+// import { useState } from 'react';
+
+// const CounterValue = ({value}) => (
+// <span>{value}</span>
+// );
+
+// const CounterBody = ({ value, decrement, increment }) => (
+//   <>
+//     <button onClick={decrement}>-</button>
+//     <CounterValue value={value}/>
+//     <button>{increment}</button>
+//   </>
+// );
+
+// const Counter = () => {
+//   const [value, setValue] = useState(10);
+//   const decrement = () => setValue(prev => prev - 1);
+//   const increment = () => setValue(prev => prev + 1);
+//   return (
+//     <div className="counter">
+//       <CounterBody value={value} decrement={decrement} increment={increment} />
+//     </div>
+//   );
+// };
+// export default Counter;
